ac3c4544951210ca891882491b1efb0bd0795560
David Fogle <david@civo.com>
2021-12-15 03:59:00 +0000

    Fully functionally broken.

--- controllers/podlister_controller.go
+++ controllers/podlister_controller.go
@@ -19,23 +19,38 @@ package controllers
 import (
 	"context"
 
+	//"k8s.io/apimachinery/pkg/labels"
+	//types "k8s.io/apimachinery/pkg/types"
+	appsv1 "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+
+	//"k8s.io/apimachinery/pkg/api/errors"
+	"github.com/go-logr/logr"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	podlisterv1alpha1 "github.com/birdiesanders/demo-operator/api/v1alpha1"
+	//"github.com/onsi/ginkgo/types"
 )
 
 // PodListerReconciler reconciles a PodLister object
 type PodListerReconciler struct {
 	client.Client
+	Log    logr.Logger
 	Scheme *runtime.Scheme
 }
 
 //+kubebuilder:rbac:groups=pod-lister.example.com,resources=podlisters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=pod-lister.example.com,resources=podlisters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=pod-lister.example.com,resources=podlisters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=core,resources=pod,verbs=get;list;watch;
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -48,8 +63,33 @@ type PodListerReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
 func (r *PodListerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
+	r.Log.Info("Starting Reconcile:", "namespace", req.NamespacedName.Namespace, "name", req.Name)
 
-	// your logic here
+	//r.Log.Info("Looking for pods with label...", "context", ctx)
+	podList := v1.PodList{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+	}
+
+	listOptions := []client.ListOption{
+		client.InNamespace(req.NamespacedName.Namespace),
+		client.MatchingLabels{"hello": "civo"},
+	}
+	err := r.List(ctx, &podList, listOptions...)
+
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+	for _, pod := range podList.Items {
+
+		r.Log.Info("Pod Found!", "pod", pod)
+		deployment := &appsv1.Deployment{}
+		r.Get(ctx, types.NamespacedName{Namespace: pod.Namespace, Name: "ngin"}, deployment)
+		r.Log.Info("Found deployment:", "deployment", deployment.Name)
+		r.Log.Info("--------------------------------------------------------------------------------------")
+	}
 
 	return ctrl.Result{}, nil
 }
